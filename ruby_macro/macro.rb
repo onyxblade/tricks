b8c6d1170169a8a2fd7ab3be68bc44d1030c1be7
Cichol <cichol@live.cn>
2015-06-08 23:47:37 +0800

    update macro

--- ruby_macro/macro.rb
+++ ruby_macro/macro.rb
@@ -1,5 +1,6 @@
 class RubyMacro
-  def self.do_not_apply
+  def self.init outer
+    @@binding = outer
   end
 
   def self.apply &p
@@ -7,15 +8,17 @@ class RubyMacro
     self.instance_eval &p
     current_file = File.expand_path($PROGRAM_NAME)
     code = File.open(current_file, 'r').read
-    code.gsub!('RubyMacro.apply', 'RubyMacro.do_not_apply')
+    code.gsub!(/RubyMacro([\w\W]*?)end/, '')
     @@gsub.each do |k, v|
       code.gsub!(k, v)
     end
-    eval code
+    @@binding.eval code
     exit
   end
 
   def self.define ori, tar
-    @@gsub[ori] = tar
+    @@gsub[ori.to_s] = tar.to_s
   end
 end
+
+RubyMacro.init binding
--- ruby_macro/z.rb
+++ ruby_macro/z.rb
@@ -1,7 +1,8 @@
 require './macro'
 
 RubyMacro.apply do
-  define "CATS", "2"
+  define :CATS, 2
 end
 
 puts CATS # => 2
+puts self # => main
